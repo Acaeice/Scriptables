105380cc43b104ebd31716dc3c7ee64e3d6bbf52
“王和俊” <wanghejun@meikeland.com>
2021-07-04 20:41:25 +0800

    添加

--- install-runtime.js
+++ install-runtime.js
@@ -1,5 +1,5 @@
 const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
-await Promise.all(['「小件件」开发环境.js', '「源码」小组件示例.js'].map(async js => {
+await Promise.all(['「小件件」开发环境.js', '「源码」小组件示例.js','「仵作」联通跳点'].map(async js => {
   const REQ = new Request(`https://gitee.com/nodewuozuo/Scriptables/raw/main/Scripts/${encodeURIComponent(js)}`);
   const RES = await REQ.load();
   FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
