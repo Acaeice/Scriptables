1d7f9393c7fa0d6c47e4ed43db56de1dee990b87
王和俊 <wanghejun@meikeland.com>
2021-07-12 15:58:49 +0800

    fix

--- install-runtime.js
+++ install-runtime.js
@@ -1,8 +1,8 @@
 const FILE_MGR = FileManager[module.filename.includes('Documents/iCloud~') ? 'iCloud' : 'local']();
-await Promise.all(['「小件件」开发环境.js', '「源码」小组件示例.js','「源码」百度热榜.js','「仵作」就跳一点.js'].map(async js => {
+await Promise.all(['「小件件」开发环境.js', '「仵作」就跳一点.js'].map(async js => {
   const REQ = new Request(`https://gitee.com/nodewuzuo/Scriptables/raw/main/Scripts/${encodeURIComponent(js)}`);
   const RES = await REQ.load();
   FILE_MGR.write(FILE_MGR.joinPath(FILE_MGR.documentsDirectory(), js), RES);
 }));
 FILE_MGR.remove(module.filename);
-Safari.open("scriptable:///open?scriptName="+encodeURIComponent('「源码」小组件示例'));
+Safari.open("scriptable:///open?scriptName=" + encodeURIComponent('「仵作」就跳一点.js'));
